7ffe494c39bb1d499b2ddce4f627e021d4e69789
Chokapik21 <milamessy28@gamil.com>
2026-09-25 12:53:26 -0500

    Arquitectura y desarrollo del backend

---
+++ OndaRetro/Desarollo/backend_OndaRetro/main.py
@@ -0,0 +1,16 @@
+# main:
+
+import uvicorn
+
+def start():
+    print("Starting server...")
+    uvicorn.run(
+                "presentation.webApiOndaRetro:app",
+                host="127.0.0.1",
+                port=7000,
+                reload=True
+                )
+    print("Server is running.")
+
+if __name__ == "__main__":
+    start()
---
+++ OndaRetro/Desarollo/backend_OndaRetro/presentation/webApiOndaRetro.py
@@ -0,0 +1,9 @@
+# webapilibreriaiupb.py
+
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="API Onda Retro",
+    description="API para la Onda Retro",
+    version="1.0.0"
+)
